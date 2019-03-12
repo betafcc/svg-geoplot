6407293bf326931ae046742c5f950bd5e9e34246
Beta Faccion <betafcc@gmail.com>
2019-03-12 13:33:03 -0300

    Integrates with svg module

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 const fs = require('fs')
 const PDFDocument = require('pdfkit')
-const svg = require('@betafcc/svg')
-const doc = new PDFDocument();
-
 
 const geoplot = require('./lib')
 
@@ -11,37 +8,36 @@ const url = 'https://raw.githubusercontent.com/betafcc/mapas-kronoos/master/ufs.
 
 
 const main = async () => {
-  doc.pipe(fs.createWriteStream('output.pdf'))
+  const doc = new PDFDocument()
+  doc.pipe(fs.createWriteStream('output.pdf'));
 
-  svg
-    .apply(
-      (await geoplot.fetch({
-        url, // a url do topojson
-        key: 'ufs' // o campo 'id' das features no topojson
-      }))
-      // pode ser usado com o mapa em arquivo local
-      // (geoplot({topo: require('./ufs.topo.json'), key: 'ufs'}))
-        .mark({point: 'PI', text: 1, dy: 2})
-        .mark({point: 'MT', text: 2})
-        .mark({point: 'DF', text: 3})
-        .mark({point: 'ES', text: 4})
-        .mark({point: {lat: -23.44, long: -46.67}, text: 6})
-        .mark({point: {lat: -21.48, long: -48.63}, text: 5})
-        .mark({
-          points: ['MS', 'PR', 'SC'],
-          destiny: c => ({x: c.x - 15, y: c.y + 5}),
-          text: 7
-        })
-        .mark({
-          points: ['CE', 'RN', 'PB', 'PE', 'AL'],
-          destiny: c => ({x: c.x - 5, y: c.y - 15}),
-          text: 8,
-        })
-    )
+  (await geoplot.fetch({
+    url, // a url do topojson
+    key: 'ufs' // o campo 'id' das features no topojson
+  }))
+  // pode ser usado com o mapa em arquivo local
+  // ao invez do campo 'url' use 'topo' e passe o objeto no formato topojson
+  // (geoplot({topo: require('./ufs.topo.json'), key: 'ufs'}))
+    .mark({point: 'PI', text: 1, dy: 2})
+    .mark({point: 'MT', text: 2})
+    .mark({point: 'DF', text: 3})
+    .mark({point: 'ES', text: 4})
+    .mark({point: {lat: -23.44, long: -46.67}, text: 6})
+    .mark({point: {lat: -21.48, long: -48.63}, text: 5})
+    .mark({
+      points: ['MS', 'PR', 'SC'],
+      destiny: c => ({x: c.x - 15, y: c.y + 5}),
+      text: 7
+    })
+    .mark({
+      points: ['CE', 'RN', 'PB', 'PE', 'AL'],
+      destiny: c => ({x: c.x - 5, y: c.y - 15}),
+      text: 8,
+    })
     .toPdfkit(doc, {x: 200, y: 100, width: 350, height: 350})
 
   doc.end()
 }
 
 
-main()
+main().catch(e => console.error(e))
